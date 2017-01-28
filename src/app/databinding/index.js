9ebe5b8e543d712aaaa40ebae8bf9b2004b81b1a
arunans23 <arunans.14@cse.mrt.ac.lk>
2017-01-28 22:57:14 +0530

    make changes

---
+++ src/app/databinding/index.js
@@ -0,0 +1,4 @@
+"use strict";
+var databinding_component_1 = require('./databinding.component');
+exports.DatabindingComponent = databinding_component_1.DatabindingComponent;
+//# sourceMappingURL=index.js.map
--- src/app/first-app.component.ts
+++ src/app/first-app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-// import { DatabindingComponent } from './databinding';
+import { DatabindingComponent } from './databinding';
 
 @Component({
   moduleId: module.id,
   selector: 'first-app-app',
-  template: '<h1>First App</h1>'
+  template: '<h1>First App</h1><p><app-databinding></app-databinding></p>'
 })
 export class FirstAppAppComponent {
 
